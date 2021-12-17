80a4859471cc5c0da965e765a89706b9637b9db4
Alessia9802 <alessiamisciagna@yahoo.it>
2021-12-17 16:01:44 +0100

    Install Bootstrap

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,8 @@
 <header>
     <p>header</p>
+    <nav class="nav justify-content-center">
+        <a class="nav-link" href="">Home</a>
+        <a class="nav-link" href="">Products</a>
+        <a class="nav-link " href="">News</a>
+    </nav>
 </header>
