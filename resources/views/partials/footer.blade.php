c37b2cdc9b4e1fa12504820e6d21f10aff6bf093
Alessia9802 <alessiamisciagna@yahoo.it>
2021-12-20 17:04:54 +0100

    Add footer section

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,77 @@
-<footer>
-    <p>footer</p>
+<footer id="site_footer">
+ <div class="section">
+      <div class="menu_footer">
+        <div class="menu_dc_comics">
+          <h2>DC Comics</h2>
+          <nav class="comics">
+            <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+          </nav>
+          <div class="menu_shop">
+            <h2>Shop</h2>
+            <nav>
+              <ul>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div class="menu_dc">
+          <h2>Dc</h2>
+          <nav>
+            <ul>
+              <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Privacy Policy(New)</a></li>
+              <li><a href="#">Ad Choises</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscriptions</a></li>
+              <li><a href="#">Talent Workshops</a></li>
+              <li><a href="#">CPSC Certificates</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">Contact us</a></li>
+            </ul>
+          </nav>
+        </div>
+        <div class="menu_sites">
+          <h2>Sites</h2>
+          <nav>
+            <ul>
+              <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="logo_footer">
+        <img
+          class="logo"
+          src="{{asset('img/dc-logo-bg.png')}}"
+          alt=""
+        />
+      </div>
+    </div>
+    <div class="copyright">
+      <div class="btn_register">Sign-up now!</div>
+      <div class="social">
+        <p>Follow us</p>
+        <img src="{{asset('img/footer-facebook.png')}}" alt="" />
+        <img src="{{asset('img/footer-twitter.png')}}" alt="" />
+        <img src="{{asset('img/footer-youtube.png')}}" alt="" />
+        <img src="{{asset('img/footer-pinterest.png')}}" alt="" />
+        <img src="{{asset('img/footer-periscope.png')}}" alt="" />
+      </div>
+    </div>
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,29 @@
         @endforeach
         <div class="load">Load More</div>
     </div>
+    <div class="icons">
+         <div class="icon">
+              <img class="img_icon" src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+             <p>digital comics</p>
+            </div>
+        <div class="icon">
+            <img class="img_icon" src="{{asset('img/buy-comics-merchandise.png')}}" alt="">
+            <p>dc merchandise</p>
+        </div>
+        <div class="icon">
+            <img class="img_icon" src="{{asset('img/buy-comics-shop-locator.png')}}" alt="">
+            <p>subscription</p>
+        </div>
+        <div class="icon">
+            <img class="img_icon" src="{{asset('img/buy-comics-subscriptions.png')}}" alt="">
+            <p>comic shop locator</p>
+        </div>
+        <div class="icon">
+            <img class="img_icon card_visa" src="{{asset('img/buy-dc-power-visa.svg')}}" alt="">
+            <p>dc power visa</p>
+        </div>
+        
+    </div>
 </div>
 
 @endsection
