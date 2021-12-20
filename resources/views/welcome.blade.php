f26e95e65bc5a909d5217ea43e33109833803073
Alessia9802 <alessiamisciagna@yahoo.it>
2021-12-20 15:50:45 +0100

    Add dc comics series

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>welcome</h1>
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente iste doloremque architecto quaerat corporis aliquid, exercitationem labore consectetur reiciendis cumque!</p>
 
-<div class="container">
-    <div class="d-flex">
+<div class="main_container">
+    <div class="jumbo"></div>
+    <div class="container_comics">
         @foreach($series as $serie)
-        <div class="">
+        <div class="cont_comics">
 
-            <div class="">
-                <img src="{{ $serie['thumb'] }}" alt="">
-                <h5 class="text-center">{{ $serie['title'] }}</h5>
+            <div class="image_wrap">
+                <img class="card" src="{{ $serie['thumb'] }}" alt="">
+                <h5 class="card_title">{{ $serie['title'] }}</h5>
 
             </div>
 
